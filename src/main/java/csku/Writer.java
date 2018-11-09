10944e7dc037d1788a5191663ab150d968848846
Yackz <40462476+Yackz@users.noreply.github.com>
2018-11-09 08:45:28 +0700

    Lab10 fix

---
+++ src/main/java/csku/Writer.java
@@ -0,0 +1,21 @@
+package csku;
+
+import java.io.*;
+
+public class Writer {
+    private static final String fileName = "account.txt";
+
+    static public void add(String detail) {
+        try {
+            FileWriter fileWriter = new FileWriter(fileName, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(detail);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
